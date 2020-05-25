957651734e7ca6a0b6fae3a6a78b8dcd3c967e1f
Aishwarya-Pupala <60351480+Aishwarya-Pupala@users.noreply.github.com>
2020-05-25 07:49:46 +0530

    Create factorial_array.java

---
+++ factorial_array.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+        long[] arr=new long[5];
+
+        long[] a=new long[5];
+
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<arr.length;i++){
+
+          int  n=sc.nextInt();
+            if(n>0){
+            arr[i]=n;}
+        }
+    long fact=1;
+     for(int i=0;i<arr.length;i++){
+          for(int j=1;j<=arr[i];j++){
+              fact=fact*j;
+          }
+          a[i]=fact;
+          fact=1;
+    }
+    
+ for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }  
+}}
